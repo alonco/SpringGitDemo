3ef465a5aeb406cc8cc0afa066024a8914d7f499
Alon <alon_co@outlook.com>
2018-07-10 16:17:38 +0300

    update main.java and demo.java

--- src/com/springbootgitdemo/Main.java
+++ src/com/springbootgitdemo/Main.java
@@ -5,70 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!!!");
-        demo();
-	changeFromGit();
-	    changeFromGit();
-	    
-	    changeFromGit();
-	    
-	    changeFromGit();
-	    
-	/* new from github*/	    
-	      System.out.println("Hello World!!!");
-        demo();
-	      System.out.println("Hello World!!!");
-        demo();
-	      System.out.println("Hello World!!!");
-        demo();
-	      System.out.println("Hello World!!!");
-        
-	    /* change method from github 2*/
-	    demo();
-	     demo();
-	      System.out.println("Hello World!!!");
-        demo();
-	     demo();
-	      System.out.println("Hello World!!!");
-        demo();
-	     demo();
-	      System.out.println("Hello World!!!");
-        demo();
-	    
-	    /*change method from github 3*/
-	     System.out.println("Hello World!!!");
-        demo();
-	changeFromGit();
-	    changeFromGit();
 
-
-        /*change method from Java*/
-        System.out.println("Hello World!!!");
-        demo();
-        changeFromGit();
-        changeFromGit();
-	    
-	       /*change method from github 4*/
-	     System.out.println("Hello World!!!");
-        demo();
-	changeFromGit();
-	    changeFromGit();
-
-        /*change method from java before commit*/
-        System.out.println("Hello World!!!");
-        demo();
-        changeFromGit();
-        changeFromGit();
-	
-	    
-	/*change method from github 5*/
-	     System.out.println("Hello World!!!");
-        demo();
-	
-	        
-	/*change method from github 6*/
-	     System.out.println("Hello World!!!");
-        demo();
-	
     }
 
     private static void demo() {
